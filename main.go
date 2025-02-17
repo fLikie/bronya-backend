9c345b6e2e8fa326be462c2455236a6daa4baff1
Arsan Timofeev <flikie3@gmail.com>
2025-02-17 22:45:13 +0500

    change db structure

--- main.go
+++ main.go
@@ -95,7 +95,6 @@ func main() {
 
 type User struct {
 	ID        uint   `gorm:"primaryKey"`
-	Username  string `gorm:"unique;not null"`
 	Email     string `gorm:"unique;not null"`
 	Password  string `gorm:"not null"`
 	Role      string `gorm:"not null;default:user"`
